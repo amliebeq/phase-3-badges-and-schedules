ff1959c3ed85ef5c0c9bb1428e3a861cdd7aeeea
alex lieberman <alex.lieberman182@gmail.com>
2023-03-25 09:04:14 -0700

    commit tree d4e37a07ec6a2ad54b0d2ea7c2512373dcd3f001

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map { |badge| "Hello, my name is #{badge}."}
+end
+
+def assign_rooms(arr)
+    arr.map.each_with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(arr)
+    batch_badge_creator(arr).each { |item| puts item}
+    assign_rooms(arr).each { |item| puts item}
+end
